2e1bd1fd6291f61afb18addd739143810e7a54a1
guiballa <guiballa@hotmail.com>
2020-03-02 12:41:47 -0300

    Aprendendo Java

--- Linguagens1_Projetos/Pratica01/aula/MeuPrograma.java
+++ Linguagens1_Projetos/Pratica01/aula/MeuPrograma.java
@@ -2,6 +2,6 @@ class MeuPrograma {
     public static void main(String[] args) {
 
         // mensagem de saida do sistema
-        System.out.println("Minha primeira aplicação Java!!");
+        System.out.println("Olá mundo!!\nEstou programando no modo Conan. :)");
     }
 }
