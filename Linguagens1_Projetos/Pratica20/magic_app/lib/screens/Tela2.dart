73d9075c626351f58a328ca14e5f5e824537d3aa
Guiballa <31780336+guiballa@users.noreply.github.com>
2020-12-01 08:17:00 -0300

    Network Helper no lugar certo

--- Linguagens1_Projetos/Pratica20/magic_app/lib/screens/Tela2.dart
+++ Linguagens1_Projetos/Pratica20/magic_app/lib/screens/Tela2.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
 import 'package:magic_app/Utilities/network_helper.dart';
+import 'package:magic_app/models/scryfall_api.dart';
 
 class SegundaTela extends StatefulWidget {
   String Nome;
@@ -14,20 +15,24 @@ class SegundaTela extends StatefulWidget {
 
 class _SegundaTelaState extends State<SegundaTela> {
   List _cardsListUrl = [];
-  var _requisicao = NetworkHelper(url:"url da sua api");
+  
 
   @override
   Widget build(BuildContext context) {
+    var requisicao = NetworkHelper(url:"https://api.scryfall.com/cards/search?q=" + Nome + "&unique=cards&order=name");
+    var data = scryfall_api.fromJson(await requisicao.getData());
     return SafeArea(
         child: Scaffold(
           appBar: AppBar(title: Text("Busca pelo nome de "+ widget.Nome , style: GoogleFonts.lato(),), ),
           body: Column(
             children: [
               Expanded(
-                  child: ListView.builder(
+                  child:
+
+                    ListView.builder(
                       itemBuilder: (context, index){
                         return ListTile(
-                          title: Text("ola"),
+                          leading: Image.network(_cardsListUrl[index]),
                         );
                       }
                   )
@@ -36,5 +41,15 @@ class _SegundaTelaState extends State<SegundaTela> {
           ),
         )
     );
+  }
+  void adicionar_novo_registro(String Nome) {
+    setState(() async {
+      var requisicao = NetworkHelper(url:"https://api.scryfall.com/cards/search?q=" + Nome + "&unique=cards&order=name");
+      var data = scryfall_api.fromJson(await requisicao.getData());
+      // var num_dados = data.totalCards;
+      // _cardsListUrl.add();
+      print(_cardsListUrl);
+    });
+
   }
 }
