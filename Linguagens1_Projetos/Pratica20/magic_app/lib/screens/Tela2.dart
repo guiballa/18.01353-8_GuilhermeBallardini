c3752a113ce07f2dc701ea9ee2e79ebf5863b212
Guiballa <31780336+guiballa@users.noreply.github.com>
2020-11-30 23:14:50 -0300

    Mudan;a pra statefull

--- Linguagens1_Projetos/Pratica20/magic_app/lib/screens/Tela2.dart
+++ Linguagens1_Projetos/Pratica20/magic_app/lib/screens/Tela2.dart
@@ -1,20 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:http/http.dart' as http;
+import 'package:magic_app/Utilities/network_helper.dart';
 
-class SegundaTela extends StatelessWidget {
+class SegundaTela extends StatefulWidget {
   String Nome;
 
+  @override
+  _SegundaTelaState createState() => _SegundaTelaState();
+
+  SegundaTela({Key key, @required this.Nome}) : super(key: key);
+}
+
+class _SegundaTelaState extends State<SegundaTela> {
   List _cardsListUrl = [];
+  var _requisicao = NetworkHelper(url:"url da sua api");
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-          appBar: AppBar(title: Text("Busca pelo nome de "+ Nome , style: GoogleFonts.lato(),), ),
+          appBar: AppBar(title: Text("Busca pelo nome de "+ widget.Nome , style: GoogleFonts.lato(),), ),
           body: Column(
             children: [
-
               Expanded(
                   child: ListView.builder(
                       itemBuilder: (context, index){
@@ -29,7 +37,4 @@ class SegundaTela extends StatelessWidget {
         )
     );
   }
-  SegundaTela({Key key, @required this.Nome}) : super(key: key);
-
-
 }
