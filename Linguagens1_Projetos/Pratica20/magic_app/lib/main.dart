b751008c06fdbc2fbd7b6d201badd6c7e0b75d42
Guiballa <31780336+guiballa@users.noreply.github.com>
2020-11-30 21:23:35 -0300

    Criando Main

--- Linguagens1_Projetos/Pratica20/magic_app/lib/main.dart
+++ Linguagens1_Projetos/Pratica20/magic_app/lib/main.dart
@@ -1,23 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:magic_app/screens/Home.dart';
 
-void main() {
-  runApp(MyApp());
-}
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: HomeScreen(),
-    );
-  }
-}
+// void main() {
+//   runApp(MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       debugShowCheckedModeBanner: false,
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//         visualDensity: VisualDensity.adaptivePlatformDensity,
+//       ),
+//       home: HomeScreen(),
+//     );
+//   }
+// }
 
+void main(){
+  runApp(MaterialApp(
+    initialRoute: '/',
+    debugShowCheckedModeBanner: false,
+    routes: {
+      '/': (context) => HomeScreen(),
+      // '/SegundaTela': (context) => History(),
+    },
+  ),
+  );
+}
