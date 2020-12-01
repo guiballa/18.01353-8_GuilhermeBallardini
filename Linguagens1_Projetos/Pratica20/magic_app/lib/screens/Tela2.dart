b0684baa02790fc55212f471e9e928096a79db5b
Guiballa <31780336+guiballa@users.noreply.github.com>
2020-11-30 22:45:14 -0300

    importando http

--- Linguagens1_Projetos/Pratica20/magic_app/lib/screens/Tela2.dart
+++ Linguagens1_Projetos/Pratica20/magic_app/lib/screens/Tela2.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:http/http.dart' as http;
 
 class SegundaTela extends StatelessWidget {
   String Nome;
