fbf1553a394a3e09989d53201931c30f53bbf715
Guiballa <31780336+guiballa@users.noreply.github.com>
2020-12-01 22:49:51 -0300

    Ajuste geral

--- P2/magic_app/lib/screens/Tela2.dart
+++ P2/magic_app/lib/screens/Tela2.dart
@@ -14,66 +14,89 @@ class SegundaTela extends StatefulWidget {
 class _SegundaTelaState extends State<SegundaTela> {
   List _cardsListUrl = [];
   NetworkHelper requisicao;
+  Scryfall_api scryfallApi = new Scryfall_api();
   @override
   void initState() {
     iniciaData();
     super.initState();
   }
 
-  iniciaData(){
+  iniciaData() {
     String card = widget.name;
     card = card.replaceAll(" ", "%20");
-    requisicao = NetworkHelper(url:"https://api.scryfall.com/cards/search?q=" +card+ "&unique=cards&order=name");
+    requisicao = NetworkHelper(
+        url: "https://api.scryfall.com/cards/search?q=" +
+            card +
+            "&unique=cards&order=name");
   }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
+        top: true,
         child: Scaffold(
-            appBar: AppBar(title: Text("Busca pelo nome de "+ widget.name , style: GoogleFonts.lato(),), ),
-            body: Column(
-              children: <Widget>[
-                FutureBuilder(
-                  future: requisicao.getData(),
-                  builder: (BuildContext context, AsyncSnapshot snapshot) {
-                    if (snapshot.hasData) {
-                      var dados = scryfall_api.fromJson(snapshot.data);
-                      return Expanded(
-                        
-                        child: ListView.builder(
-                            itemCount: 2,
-                            itemBuilder: (context, index){
-                              return ListTile(
-                                title: Text("teste"),
-                                leading: Image.network(dados.data[1].imageUris.small),
-
-                                // leading: Image.network(_cardsListUrl[index]),
-                              );
-                            }),
-                      );
-                    } else {
-                      return Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Center(child: Text('Buscando cartas', style: TextStyle(fontSize: 24, ),)),
-                      );
-
-                    }
-                  },
-                ),
-              ],
-            )
-        )
-    );
+            body: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    children: [
+                      IconButton(
+                          icon: Icon(
+                            Icons.arrow_back_ios,
+                            size: 24,
+                          ),
+                          onPressed: () => Navigator.pop(context)),
+                      Expanded(
+                          child: Container(
+                            alignment: Alignment.center,
+                            child: Text(
+                              widget.name,
+                              style: GoogleFonts.lato(),
+                            ),
+                          ))
+                    ],
+                  ),
+                  FutureBuilder(
+                    future: requisicao.getData(),
+                    builder: (BuildContext context, AsyncSnapshot snapshot) {
+                      if (snapshot.hasData) {
+                        scryfallApi = Scryfall_api.fromJson(snapshot.data);
+                        return Expanded(
+                            child: GridView.builder(
+                              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                                crossAxisCount: 2,
+                                mainAxisSpacing: 16,
+                                crossAxisSpacing: 16,
+                              ),
+                              itemCount: scryfallApi.totalCards,
+                              itemBuilder: (context, index) {
+                                Data data = scryfallApi.data[index];
+                                if (data.imageUris == null) print(index);
+                                return Container(
+                                  decoration: BoxDecoration(
+                                      image: DecorationImage(
+                                          image: NetworkImage(data.imageUris.large))),
+                                );
+                              },
+                            ));
+                      } else {
+                        return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Center(
+                              child: Text(
+                                'Buscando cartas',
+                                style: TextStyle(
+                                  fontSize: 24,
+                                ),
+                              )),
+                        );
+                      }
+                    },
+                  ),
+                ],
+              ),
+            )));
   }
-
-
-
-
-  void adicionar_nova_carta(String Nome, int index) {
-    setState(() async {
-      var requisicao = NetworkHelper(url:"https://api.scryfall.com/cards/search?q=" + Nome + "&unique=cards&order=name");
-      var dados = scryfall_api.fromJson(await requisicao.getData());
-       _cardsListUrl.add(dados.data[index].imageUris.large);
-    });
-  }
+  
 }
