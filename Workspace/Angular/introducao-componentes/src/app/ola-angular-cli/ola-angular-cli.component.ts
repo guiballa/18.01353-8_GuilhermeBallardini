ed3e59b5f592b89643b8749100d53d350a4dfed3
Guiballa <31780336+guiballa@users.noreply.github.com>
2021-03-27 11:58:53 -0300

    Agora mostrando o cli funcionando

--- Workspace/Angular/introducao-componentes/src/app/ola-angular-cli/ola-angular-cli.component.ts
+++ Workspace/Angular/introducao-componentes/src/app/ola-angular-cli/ola-angular-cli.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-ola-angular-cli',
   templateUrl: './ola-angular-cli.component.html',
   styleUrls: ['./ola-angular-cli.component.css']
 })
-export class OlaAngularCliComponent implements OnInit {
+export class OlaAngularCliComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
