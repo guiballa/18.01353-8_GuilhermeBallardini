5be860bb26802cde0a0086397fcbd2e236fea5ef
Guiballa <31780336+guiballa@users.noreply.github.com>
2021-04-24 11:06:20 -0300

    usando accordeion

--- Workspace/app-clientes/src/app/app.module.ts
+++ Workspace/app-clientes/src/app/app.module.ts
@@ -4,19 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ClienteInserirComponent } from './clientes/cliente-inserir/cliente-inserir.component';
+import { CabecalhoComponent } from './cabecalho/cabecalho.component';
+import { ClienteListaComponent } from './clientes/cliente-lista/cliente-lista.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-import { CabecalhoComponent } from './cabecalho/cabecalho.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @NgModule({
   declarations: [
     AppComponent,
     ClienteInserirComponent,
-    CabecalhoComponent
+    CabecalhoComponent,
+    ClienteListaComponent
   ],
   imports: [
     FormsModule,
@@ -26,6 +29,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatInputModule,
     MatButtonModule,
     MatToolbarModule,
+    MatExpansionModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
