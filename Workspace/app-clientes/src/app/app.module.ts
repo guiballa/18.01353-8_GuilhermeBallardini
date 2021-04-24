03c00dfb8a49c40e57c266ec454357b6e738e3c9
Guiballa <31780336+guiballa@users.noreply.github.com>
2021-04-24 10:41:50 -0300

    Importando modulos

--- Workspace/app-clientes/src/app/app.module.ts
+++ Workspace/app-clientes/src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ClienteInserirComponent } from './clientes/cliente-inserir/cliente-inserir.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClienteInserirComponent
   ],
   imports: [
-    BrowserModule
+    FormsModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
